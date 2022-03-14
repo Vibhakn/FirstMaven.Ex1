0d4a2929d00efc124f10c6baf28f9f0386298322
Vibhakn <vibhakn4@gmail.com>
2022-03-14 17:25:24 +0530

    second commit

--- FirstMaven.Ex1/src/main/java/com/lti/demos/Calculator.java
+++ FirstMaven.Ex1/src/main/java/com/lti/demos/Calculator.java
@@ -4,6 +4,8 @@ public class Calculator {
 	public int add(int x, int y)
 	{
         System.out.println("hi there");
+        System.out.println("hi there");
+
 
 		int result=x+y;
 		return result;
